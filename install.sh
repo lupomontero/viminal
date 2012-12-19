47ff1131122dd998eca62f0f72b9eeefc5876cd1
root <root@puppet.sa-east-1.enoi.se>
2012-12-19 03:56:52 +0000

    ...

---
+++ install.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env sh
+
+src="$HOME"/.viminal
+repo=git@github.com:lupomontero/viminal.git
+vimrc="$HOME"/.vimrc
+vim="$HOME"/.vim
+
+if [ -d "$src" ]; then
+  rm -rf "$src"
+fi
+
+git clone git@github.com:lupomontero/viminal.git "$src"
+cd "$src"
+git submodule init
+git submodule update
+
+if [ -h "$vimrc" ]; then
+  rm "$vimrc"
+elif [ -f "$vimrc" ]; then
+  mv "$vimrc" "$vimrc".bk
+fi
+
+if [ -h "$vim" ]; then
+  rm "$vim"
+elif [ -d "$vim" ]; then
+  mv "$vim" "$vim".bk
+fi
+
+ln -s "${src}/vimrc" "$vimrc"
+ln -s "${src}/vim" "$vim"
+
+exit 0
+
